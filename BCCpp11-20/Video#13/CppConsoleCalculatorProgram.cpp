ad2e9ce645442f48755f4d0e0a2a40502ab18b29
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-21 12:11:33 -0700

    --Building out arithmetic calculator in Cpp

--- BCCpp11-20/Video#13/CppConsoleCalculatorProgram.cpp
+++ BCCpp11-20/Video#13/CppConsoleCalculatorProgram.cpp
@@ -3,6 +3,26 @@
 
 int main() {
 
-    return 0;
+    char op;
+    double num1;
+    double num2;
+    double result;
+
+    std::cout << "********** CALCULATOR **********\n";
+
+    std::cout << "Enter either (+ - * /): ";
+    std::cin >> op;
+
+    std::cout << "Enter #1: ";
+    std::cin >> num1;
+
+    std::cout << "Enter #2: ";
+    std::cin >> num2;
+
     
+
+    std::cout << "******************************";
+
+    return 0;
+
 }
