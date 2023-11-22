01d3893223d5576eac01b389c92db27fb1fe133b
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-21 17:45:48 -0700

    using all logical operators with refactored exampl

--- BCCpp11-20/Video#15/LogicalOperatorsin.cpp
+++ BCCpp11-20/Video#15/LogicalOperatorsin.cpp
@@ -8,7 +8,23 @@ int main() {
         // || = check if at least one of the two conditions is true
         // ! = reverses the logical state of its operand
 
+    int temp;
+
+    std::cout << "Enter the temperature: ";
+    std::cin >> temp;
+
+    // MARK: - Or Logical Operator Example: '||'
+
     
+
+
+    // MARK: - And Logical Operator Example: '&&'
+
+    /* if (temp > 0 && temp < 30) {
+        std::cout << "The temperature is good!";
+    } else {
+        std::cout << "The temperature is bad!";
+    } */
 
     return 0;
 
