7d222aebc529f2bb0891a3505c5d32ddf7a805d4
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-21 17:39:34 -0700

    --Start of Logical Operators in Cpp

--- BCCpp11-20/Video#15/LogicalOperatorsin.cpp
+++ BCCpp11-20/Video#15/LogicalOperatorsin.cpp
@@ -3,8 +3,14 @@
 
 int main() {
 
+    // MARK: - Key Notes
+        // && = check if two conditions are true
+        // || = check if at least one of the two conditions is true
+        // ! = reverses the logical state of its operand
+
+    
 
     return 0;
 
-    
+
 }
