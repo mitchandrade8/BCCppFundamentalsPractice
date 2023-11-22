e4e72640ba4d9d01331d660136d2b66283047714
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-22 11:38:31 -0700

    --Length String Method in Cpp

--- BCCpp11-20/Video#17/UsefulStringMethodsin.cpp
+++ BCCpp11-20/Video#17/UsefulStringMethodsin.cpp
@@ -3,6 +3,22 @@
 
 int main() {
 
+    std::string name;
+
+    std::cout << "Enter your name: ";
+    std::getline(std::cin, name);
+
+    // MARK: - Empty String Method:
+
+    
+
+    // MARK: - Length String Method:
+
+    /* if (name.length() > 12) {
+        std::cout << "Your name can not be over 12 characters!";
+    } else {
+        std::cout << "Welcome " << name;
+    } */
 
     return 0;
 }
