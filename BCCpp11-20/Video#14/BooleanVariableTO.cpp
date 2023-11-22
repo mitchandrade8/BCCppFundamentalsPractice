61aad16fc60454da0e5b456812c32798f92c2860
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-21 17:35:58 -0700

    --Planning out the remaining vids to #20

--- BCCpp11-20/Video#14/BooleanVariableTO.cpp
+++ BCCpp11-20/Video#14/BooleanVariableTO.cpp
@@ -5,7 +5,9 @@ int main() {
 
     bool hungry = true;
 
-    hungry ? std::cout << "You are hungry" : std::cout << "You are full";
+    std::cout << (hungry ? "You are hungry" : "You are full");
+
+    // hungry ? std::cout << "You are hungry" : std::cout << "You are full";
 
     return 0;
 
